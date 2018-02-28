f735a262ddab25ee825f99258c738450568b8714
chao_wang <chao_wang@htc.com>
2018-02-28 14:51:36 +0800

    add date picker

--- app/src/main/java/com/hlwu/myapp/ui/MainActivity.java
+++ app/src/main/java/com/hlwu/myapp/ui/MainActivity.java
@@ -1,5 +1,7 @@
 package com.hlwu.myapp.ui;
 
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.ColorDrawable;
@@ -11,6 +13,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
@@ -21,11 +24,17 @@ import android.view.MenuItem;
 import android.view.View;
 
 import android.support.design.widget.TabLayout;
+import android.widget.DatePicker;
 import android.widget.ProgressBar;
 
 import com.hlwu.myapp.R;
 import com.hlwu.myapp.ui.about.AboutActivity;
 import com.hlwu.myapp.ui.dailynewslist.DailyNewsFragment;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, TabLayout.OnTabSelectedListener {
 
@@ -87,6 +96,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 @Override
                 public boolean onMenuItemClick(MenuItem item) {
                     switch (item.getItemId()) {
+                        case R.id.toolbar_menu_tools:
+                            showTools();
+                            break;
                         case R.id.toolbar_menu_about:
                             startActivity(new Intent(MainActivity.this, AboutActivity.class));
                             break;
@@ -102,6 +114,92 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mTabLayout.setVisibility(View.GONE);
         mViewPager.setVisibility(View.INVISIBLE);
+    }
+
+    private void showTools() {
+        List<String> tools = Arrays.asList(getResources().getStringArray(R.array.tools_list));
+        String items[] = new String[tools.size()];
+        for (int i = 0; i < tools.size(); i++) {
+            items[i] = tools.get(i);
+        }
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                switch (which) {
+                    case 0:
+                        showBirthdayPicker();
+                        break;
+                    case 1:
+                        break;
+                }
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void showBirthdayPicker() {
+        DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                Calendar from = Calendar.getInstance();
+                Calendar end = Calendar.getInstance();
+                from.set(year, monthOfYear, dayOfMonth, 0, 0, 0);
+                from.set(Calendar.MILLISECOND, 0);
+                end.set(Calendar.HOUR_OF_DAY, 0);
+                end.set(Calendar.MINUTE, 0);
+                end.set(Calendar.SECOND, 0);
+                end.set(Calendar.MILLISECOND, 0);
+                int days = (int) getDays(from, end);
+                showLivedDaysMessage(days, year, monthOfYear, dayOfMonth,
+                        (monthOfYear == end.get(Calendar.MONTH) && (dayOfMonth == end.get(Calendar.DAY_OF_MONTH))));
+            }
+        };
+        Calendar c = Calendar.getInstance();
+        DatePickerDialog d = new DatePickerDialog(MainActivity.this, mDateSetListener, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH))/*.setTitle("choice your birthday")*//*.show()*/;
+        d.setTitle(R.string.lived_days_title);
+        d.show();
+    }
+
+    private void showLivedDaysMessage(int days, int year, int monthOfYear,int dayOfMonth, boolean todayIsBirthday) {
+        StringBuffer sb = new StringBuffer();
+        sb.append(year).append("-").append(monthOfYear + 1).append("-").append(dayOfMonth);
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        if (todayIsBirthday) {
+            builder.setTitle(R.string.lived_days_msg_today_is_birthday);
+        }
+        if (days < 0) {
+            builder.setMessage(getResources().getString(R.string.lived_days_msg_1, sb.toString()));
+        } else if (days < 700) {
+            builder.setMessage(getResources().getString(R.string.lived_days_msg_2, days));
+        } else if (days < 3650) {
+            builder.setMessage(getResources().getString(R.string.lived_days_msg_3, days));
+        } else if (days < 7300) {
+            builder.setMessage(getResources().getString(R.string.lived_days_msg_4, days));
+        } else if (days < 18000) {
+            builder.setMessage(getResources().getString(R.string.lived_days_msg_5, days));
+        } else {
+            builder.setMessage(getResources().getString(R.string.lived_days_msg_6, days));
+        }
+        if (monthOfYear == 1 && dayOfMonth == 29) {
+            builder.setMessage(getResources().getString(R.string.lived_days_msg_7, days));
+        }
+        builder.create().show();
+    }
+
+    private double getDays(Calendar c1, Calendar c2) {
+        Date date1 = c1.getTime();
+        Date date2 = c2.getTime();
+        return getDays(date1, date2);
+    }
+
+    private double getDays(Date date1, Date date2) {
+        long time1 = date1.getTime();
+        long time2 = date2.getTime();
+        double d = (time2 - time1) / 1000d / 60 / 60 / 24;
+        return d;
     }
 
     public ProgressBar getmProgressBar() {
